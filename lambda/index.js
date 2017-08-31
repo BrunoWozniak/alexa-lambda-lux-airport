46884fcf566c60825985ac7e2e3b121afb78e956
BrunoWozniak <bruno.wozniak@gmail.com>
2017-08-31 23:23:06 +0200

    Refactored yet a bit more and changed :tell to :ask also in case of empty result

--- lambda/index.js
+++ lambda/index.js
@@ -11,7 +11,10 @@ exports.handler = function (event, context) {
 const handlers = {
 
   'LaunchRequest': function () {
-    this.emit(':ask', 'Welcome to Lux Airport! You can ask me the next flight arrival or departure at Luxembourg airport', 'What can I do for you?');
+    this.emit(':ask',
+      'Welcome to Lux Airport! You can ask me the next flight arrival or ' +
+      'departure at Luxembourg airport',
+      'What can I do for you?');
   },
 
   'Hello': function () {
@@ -19,40 +22,44 @@ const handlers = {
   },
 
   'NextDeparture': function () {
-    var nextDepartureURL = `https://api.tfl.lu/v1/Airport/Departures`;
-    var airportChimeStartSound = '<audio src="https://s3-eu-west-1.amazonaws.com/lux-airport/AirportChimeStart.mp3" />';
+    const nextDepartureURL = 'https://api.tfl.lu/v1/Airport/Departures';
+    const airportChimeStartSound = '<audio src="https://s3-eu-west-1.amazonaws.com/lux-airport/AirportChimeStart.mp3" />';
 
     axios.get(nextDepartureURL).then((response) => {
         if (response.data.length === 0) {
           throw new Error('I could not find any flight');
         }
         if (response.data[0].airline.toUpperCase() === 'LUXAIR') {
-          response.data[0].airline = 'Lux Air';
+           // Should do a map first
+           response.data[0].airline = 'Lux Air';
         }
-        var message = `Next departure is flight number
-         <say-as interpret-as="characters">${response.data[0].flight}</say-as>,
-         to, ${response.data[0].destination}, on, ${response.data[0].airline}.`;
+        const message = 'Next departure is flight number' +
+         `<say-as interpret-as="characters">${response.data[0].flight}</say-as>, ` +
+         `to, ${response.data[0].destination}, on, ${response.data[0].airline}.`;
         this.emit(':ask', `${airportChimeStartSound} ${message}`, 'What can I do for you?');
     }).catch((e) => {
-        this.emit(':tell', `${airportChimeStartSound} ${e.message}`);
+        this.emit(':ask', `${airportChimeStartSound} ${e.message}`, 'What can I do for you?');
     });
   },
 
   'NextArrival': function () {
-    var nextArrivalURL = `https://api.tfl.lu/v1/Airport/Arrivals`;
-    var airportChimeStartSound = '<audio src="https://s3-eu-west-1.amazonaws.com/lux-airport/AirportChimeStart.mp3" />';
+    const nextArrivalURL = 'https://api.tfl.lu/v1/Airport/Arrivals';
+    const airportChimeStartSound = '<audio src="https://s3-eu-west-1.amazonaws.com/lux-airport/AirportChimeStart.mp3" />';
 
     axios.get(nextArrivalURL).then((response) => {
         if (response.data.length === 0) {
           throw new Error('I could not find any flight');
         }
         if (response.data[0].airline.toUpperCase() === 'LUXAIR') {
-          response.data[0].airline = 'Lux Air';
+           // Should do some map first
+           response.data[0].airline = 'Lux Air';
         }
-        var message = `Next arrival is flight number <say-as interpret-as="characters">${response.data[0].flight}</say-as>, from, ${response.data[0].destination}, on, ${response.data[0].airline} `;
+        const message = 'Next arrival is flight number <say-as interpret-as="characters">' +
+         `${response.data[0].flight}</say-as>, from, ${response.data[0].destination}, ` +
+         `on, ${response.data[0].airline} `;
         this.emit(':ask', `${airportChimeStartSound} ${message}`, 'What can I do for you?');
     }).catch((e) => {
-        this.emit(':tell', `${airportChimeStartSound} ${e.message}`);
+        this.emit(':ask', `${airportChimeStartSound} ${e.message}`, 'What can I do for you?');
     });
   }
 
